e2ef903c2866664532075eef85e3178ab59163ac
SushiCr3ator <andreazkurt.prado@gmail.com>
2025-11-12 09:38:29 +0100

    bbb

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    float num = 5 % 2;
+    printf("%d\n", num);
+    char wword[20]="Merlin\0";
+    if (wword=="Merlin") {
+        printf("lol\n");
+    }
+    return 0;
+    // testy grr
+    //final test
+}
